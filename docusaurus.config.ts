5b47c6b5cb39461c5fdaf4313f49c37e291e822e
Hoang Le <le.dinhhoang.1207@gmail.com>
2026-04-30 10:34:57 +0700

    fix: update site URL and base path for deployment configuration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -58,10 +58,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://Planthor.github.io',
+  url: 'https://docs.planthor.space',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/planthor-documentation/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
